d632f14d89c93ba95635227d45253744ff525560
Mathias Gug <mathias@zimride.com>
2013-01-10 23:20:42 +0000

    Disable newrelic sysmond for now.

--- conf/heroku_web.sh
+++ conf/heroku_web.sh
@@ -4,6 +4,7 @@ vendor_dir="/app/vendor"
 apache_dir="${vendor_dir}/apache"
 php_dir="${vendor_dir}/php"
 
+# tailFile filename [prefix]
 tailFile() {
     local fileName
     local prefix
@@ -22,12 +23,27 @@ export LD_LIBRARY_PATH="${php_dir}/ext"
 
 if [ "${NEWRELIC_LICENSE_KEY}" != "" ]
 then
+    echo -n "Enabling NewRelic.."
     newrelic_dir="${vendor_dir}/newrelic"
+    # Setup newrelic php extension
+    # NB: the newrelic daemon will automatically be started by the php extension
     newrelic_php_conf="${php_dir}/etc/ext.d/newrelic.ini"
     tailFile "${newrelic_dir}/newrelic-daemon.log" newrelic-daemon
     tailFile "${newrelic_dir}/php_agent.log" newrelic-php-agent
     sed -i -e "s/^newrelic\.enabled.*/newrelic.enabled = true/" "${newrelic_php_conf}"
     sed -i -e "s/^newrelic\.license.*/newrelic.license = \"${NEWRELIC_LICENSE_KEY}\"/" "${newrelic_php_conf}"
+    echo -n " php.."
+    # Setup newrelic system monitoring
+    #
+    # Disable nrsysmond for now. An error is printed:
+    #   [29/timer] error: IA/FSLIST failed
+    # 
+    #newrelic_nrsysmond_cfg="${newrelic_dir}/nrsysmond.cfg"
+    #tailFile "${newrelic_dir}/nrsysmond.log" newrelic-nrsysmond
+    #sed -i -e "s/^license_key.*/license_key=\"${NEWRELIC_LICENSE_KEY}\"/" "${newrelic_nrsysmond_cfg}"
+    #"${newrelic_dir}/nrsysmond" -c "${newrelic_nrsysmond_cfg}"
+    #echo -n " nrsysmond.."
+    echo " Done!"
 fi
 
 echo "Launching apache"
