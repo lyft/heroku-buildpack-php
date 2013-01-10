d7d70700a8792ddd8e4ed5aa34b011ee0bc0a949
Mathias Gug <mathias@zimride.com>
2013-01-10 22:15:47 +0000

    Log php errors to its own file
    Fix output bufferring issue while tailing various log files.

--- conf/heroku_web.sh
+++ conf/heroku_web.sh
@@ -1,27 +1,31 @@
 set -e
-[ "${DEBUG_WEB_BOOT}" == "1" ] && set -x
 
 vendor_dir="/app/vendor"
 apache_dir="${vendor_dir}/apache"
 php_dir="${vendor_dir}/php"
 
-touch "${apache_dir}/logs/error_log"
-tail -F "${apache_dir}/logs/error_log" | sed -e 's/^/apache-error /' &
-touch "${apache_dir}/logs/access_log"
-tail -F "${apache_dir}/logs/access_log" | sed -e 's/^/apache-access /' &
-touch "${apache_dir}/logs/access_log.json"
-tail -F "${apache_dir}/logs/access_log.json" | sed -e 's/^/apache-access-json /' &
+tailFile() {
+    local fileName
+    local prefix
+    fileName="${1?filename missing}"
+    prefix="${2=$(basename ${fileName})}"
+    touch "${fileName}"
+    tail -F "${fileName}" | sed  --unbuffered -e "s/^/${prefix} /" &
+}
 
+tailFile "${apache_dir}/logs/error_log" apache-error-log
+tailFile "${apache_dir}/logs/access_log" apache-access-log
+tailFile "${apache_dir}/logs/access_log.json" apache-access-log-json
+
+tailFile "${php_dir}/php_errors.log" php-errors
 export LD_LIBRARY_PATH="${php_dir}/ext"
 
 if [ "${NEWRELIC_LICENSE_KEY}" != "" ]
 then
     newrelic_dir="${vendor_dir}/newrelic"
     newrelic_php_conf="${php_dir}/etc/ext.d/newrelic.ini"
-    touch "${newrelic_dir}/newrelic-daemon.log"
-    tail -F "${newrelic_dir}/newrelic-daemon.log" | sed -e 's/^/newrelic-daemon /' &
-    touch "${newrelic_dir}/php_agent.log"
-    tail -F "${newrelic_dir}/php_agent.log" | sed -e 's/^/newrelic-php-agent /' &
+    tailFile "${newrelic_dir}/newrelic-daemon.log" newrelic-daemon
+    tailFile "${newrelic_dir}/php_agent.log" newrelic-php-agent
     sed -i -e "s/^newrelic\.enabled.*/newrelic.enabled = true/" "${newrelic_php_conf}"
     sed -i -e "s/^newrelic\.license.*/newrelic.license = \"${NEWRELIC_LICENSE_KEY}\"/" "${newrelic_php_conf}"
 fi
