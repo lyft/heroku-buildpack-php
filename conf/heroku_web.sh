f18ce7c008943ab8d5a0b1e35f4715c40672ee11
Mathias Gug <mathias@zimride.com>
2013-01-10 02:59:18 +0000

    Support newRelic license key

--- conf/heroku_web.sh
+++ conf/heroku_web.sh
@@ -11,5 +11,19 @@ tail -F "${apache_dir}/logs/access_log.json" | sed -e 's/^/apache-access-json /'
 
 export LD_LIBRARY_PATH="${php_dir}/ext"
 
+if [ "${NEWRELIC_LICENSE_KEY}" != "" ]
+then
+    newrelic_dir="${vendor_dir}/newrelic"
+    newrelic_php_conf="${php_dir}/etc/ext.d/newrelic.ini"
+    touch "${newrelic_dir}/newrelic-daemon.log"
+    tail -F "${newrelic_dir}/newrelic-daemon.log" | sed -e 's/^/newrelic-daemon /' &
+    touch "${newrelic_dir}/php_agent.log"
+    tail -F "${newrelic_dir}/php_agent.log" | sed -e 's/^/newrelic-php-agent /' &
+    sed -i -e "s/^newrelic.enabled.*/newrelic.enabled = true" "${newrelic_php_conf}"
+    sed -i -e "s/^newrelic.license.*/newrelic.license = \"${NEWRELIC_LICENSE_KEY}\"/" "${newrelic_php_conf}"
+else
+    sed -i -e "s/^newrelic.enabled.*/newrelic.enabled = false" "${newrelic_php_conf}"
+fi
+
 echo "Launching apache"
 exec "${apache_dir}/bin/httpd" -DNO_DETACH
