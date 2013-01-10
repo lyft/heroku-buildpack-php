98eefc6f3b299c417b0600d5fa1a412933ef3114
Mathias Gug <mathias@zimride.com>
2013-01-10 00:32:10 +0000

    Update apache logging.

--- conf/heroku_web.sh
+++ conf/heroku_web.sh
@@ -2,6 +2,13 @@ vendor_dir="/app/vendor"
 apache_dir="${vendor_dir}/apache"
 php_dir="${vendor_dir}/php"
 
+touch "${apache_dir}/logs/error_log"
+tail -F "${apache_dir}/logs/error_log" | sed -e 's/^/apache-error /' &
+touch "${apache_dir}/logs/access_log"
+tail -F "${apache_dir}/logs/access_log" | sed -e 's/^/apache-access /' &
+touch "${apache_dir}/logs/access_log.json"
+tail -F "${apache_dir}/logs/access_log.json" | sed -e 's/^/apache-access-json /' &
+
 export LD_LIBRARY_PATH="${php_dir}/ext"
 
 echo "Launching apache"
